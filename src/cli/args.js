aaac9c927924ba22846003bc5e2f39e646350cf8
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-06-05 01:25:23 +0500

    Solve cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,25 @@
+// WARN! Not effecient solution, but is possible to do
+// Use instead Array.map() or for
 export const parseArgs = () => {
-    // Write your code here 
+  // Get custom args
+  const args = process.argv.slice(2);
+
+  // Collect all values to string
+  let result = '';
+  let i = 0;
+  while (i < args.length) {
+    // Select two args
+    result += `${args[i]} is ${args[i + 1]}`;
+
+    // Iterate bypass second arg
+    i += 2;
+
+    // Add separator if not the end of array
+    if (i < args.length) result += ', ';
+  }
+
+  // Print result
+  console.log(result);
 };
+
+parseArgs();
