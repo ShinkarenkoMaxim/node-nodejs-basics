4a71e55cf9c89bfd69750fbbdacbef5984f2220e
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-06-05 19:23:33 +0500

    Solve streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,39 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  // Get __dirname by parsing module url
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  // Get our text file
+  const fileEncoding = 'utf-8'; // Can change if needed
+  const fileName = 'fileToRead'; // Can change if needed
+  const filePath = path.resolve(__dirname, `./files/${fileName}.txt`);
+
+  // Create Readable stream
+  const stream = fs.createReadStream(filePath);
+
+  // Checking errors in advance
+  stream.on('error', (err) => {
+    console.log(`Reading error: ${err.message}`);
+  });
+
+  // Save data by chunks to variable
+  let body = '';
+  stream.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  // If stream ending - write to stdout as specified in assignment
+  stream.on('end', () => {
+    process.stdout.write(body + '\n'); // Add new line tag for normally displays raw data
+  });
 };
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,59 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import * as readline from 'readline';
+
 export const write = async () => {
-    // Write your code here 
+  // Write your code here
+  // Get __dirname by parsing module url
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  // Get our text file
+  const fileName = 'fileToWrite'; // Can change if needed
+  const filePath = path.resolve(__dirname, `./files/${fileName}.txt`);
+
+  // Create Writeable stream
+  const stream = fs.createWriteStream(filePath);
+
+  // Checking errors in advance
+  stream.on('error', (err) => {
+    console.log(`Writing error: ${err.message}`);
+  });
+
+  // Use readline for getting data
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Ваш текст: ',
+  });
+
+  rl.prompt();
+
+  // Handle exit program and notify
+  rl.on('close', () => {
+    stream.end();
+    console.log(`\nВсе изменения сохранены в файле: ${filePath}\n`);
+  });
+
+  // Write entered text to stream
+  rl.on('line', (input) => {
+    // Shortcut for close program
+    if (input === 'exit') {
+      stream.end();
+      rl.close();
+
+      return;
+    }
+
+    const line = input + '\n';
+    stream.write(line);
+    rl.prompt();
+  });
 };
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+write();
